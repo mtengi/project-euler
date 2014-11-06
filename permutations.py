60edac51c625665cc841e8ae0d7a5d9af175b735
mtengi <markat@princeton.edu>
2014-11-06 01:46:08 -0500

    added readme

--- permutations.py
+++
@@ -1,8 +0,0 @@
-def permute(left, sub):
-	if len(sub)==1:
-		print left + sub
-	for x in xrange(len(sub)):
-		permute(left+sub[x], sub[:x]+sub[x+1:])
-
-if __name__=="__main__":
-	permute('', 'perm')
